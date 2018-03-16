2be4cd86e2833abf89e3bbeffb51b0570ca5b82e
Északi tömb <gomokukac@gmail.com>
2018-03-16 14:40:03 +0100

    Create TestCoin.sol

---
+++ TestCoin.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.0;
+
+contract TestCoin {
+    
+    mapping (address => uint256) public balanceOf;
+    
+    function TestCoin(uint256 initialSupply) public {
+        balanceOf[msg.sender] = initialSupply;
+    }
+    
+    function transfer(address _to, uint256 _value) public {
+        require(balanceOf[msg.sender] >= _value);
+        require(balanceOf[_to] + _value >= balanceOf[_to]);
+        
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+    }
+    
+}
